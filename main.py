4b83b27c7c2461e00b218652845a21a346882090
Tony <antoniustran2020@u.northwstern.edu>
2020-03-08 17:56:26 -0400

    Updated test
    test for message to show up updated

--- main.py
+++ main.py
@@ -1,7 +1,62 @@
-from flask import Flask
+project_id = 'bigquery-266522'
+compute_region = 'us-central1'
+model_display_name = 'Turnover_20200209074919'
+inputs = {'Effective_Date': '2018-04-18 16:48:37', 
+            'Work_City':'Rockton','Age':25.1, 
+            'Job_Title':'Prodctn/Maint, Prodctn Worker', 
+            'Length_of_Service':1.2, 
+            'Job_Function': 'Operations', 
+            'Work_State':'Illinois', 
+            'Job_Family':'Prodctn Worker', 
+            'Category': 'Production Maintenance', 
+            'Major_Bus_Node_Name':'Building Solutions & Services', 
+            'Ethnicity':'White (Not Hispanic or Latino)', 
+            'Grade':'PM', 'Gender':'Male', 
+            'Record_Source': 'WORKDAY', 
+            'Work_Country':'United States'}
+
+feature_importance = None
+
+from google.cloud import automl_v1beta1 as automl
+
+client = automl.TablesClient(project=project_id, region=compute_region)
+
+if feature_importance:
+    response = client.predict(
+        model_display_name=model_display_name,
+        inputs=inputs,
+        feature_importance=True,
+    )
+else:
+    response = client.predict(
+        model_display_name=model_display_name, inputs=inputs
+    )
+
+data = {}
+
+for result in response.payload:
+    data[str(result.tables.value.string_value)] = result.tables.score
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def hello():
-    return "Hello World!"
+    return jsonify(Predictions=data)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
+© 2020 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
